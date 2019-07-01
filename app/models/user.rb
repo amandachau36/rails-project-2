c57243b8bb6703a3619aecec4139dc5038da0c1b
Amanda Chau <amandachau36@gmail>
2019-07-02 09:31:37 +1000

    validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :listings
   has_many :comments
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+
   # necessary for bcrpyt and to link password to password_digest
+  # also necessary for knock
   has_secure_password
 end
